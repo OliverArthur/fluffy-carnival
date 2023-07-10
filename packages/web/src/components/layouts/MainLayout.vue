774aee386dd80463f85b8c958c2abedb4a8a6053
Oliver Arthur <OliverArthur@users.noreply.github.com>
2023-07-10 23:38:28 +0200

    complete navbar element

--- packages/web/src/components/layouts/MainLayout.vue
+++ packages/web/src/components/layouts/MainLayout.vue
@@ -1,20 +1,51 @@
 <script lang="ts" setup>
+import { ref } from 'vue'
 import { TvContainer } from '@components/container'
 import { TvHeader } from '@components/header'
 import { TvButton } from '@components/button'
 import { TvAutocomplete } from '@components/autocomplete'
+import { TvNavbar, TvNavbarItem } from '@components/navbar'
+
+// TODO: call the API to get the series.
+const setGenre = (genre: string) => {
+	console.log(genre)
+}
+
+const navbarOpen = ref(true)
+
+const toggleNavbar = () => {
+	navbarOpen.value = !navbarOpen.value
+}
+
 </script>
 <template>
 	<tv-header>
 		<template #burger-button>
-			<tv-button variant="icon" icon-name="menu" />	
+			<tv-button @on-pressed="toggleNavbar " variant="icon" icon-name="menu" />
 		</template>
-		<template #logo>
-			SeriesTube
-		</template>
+		<template #logo> SeriesTube </template>
 		<template #content>
-			<tv-autocomplete :items="['banana', 'orange', 'pineapple', 'grape', 'strawberry', 'watermelon', 'melon', 'papaya']" />
+			<tv-autocomplete
+				:items="[
+					'banana',
+					'orange',
+					'pineapple',
+					'grape',
+					'strawberry',
+					'watermelon',
+					'melon',
+					'papaya'
+				]"
+			/>
 		</template>
 	</tv-header>
+	<tv-navbar :is-open="navbarOpen">
+		<tv-navbar-item @on:action="setGenre('action')"> Action </tv-navbar-item>
+		<tv-navbar-item @on:action="setGenre('adventure')"> Adventure </tv-navbar-item>
+		<tv-navbar-item @on:action="setGenre('drama')"> Drama </tv-navbar-item>
+		<tv-navbar-item @on:action="setGenre('horror')"> Horror </tv-navbar-item>
+		<tv-navbar-item @on:action="setGenre('mystery')"> Mystery </tv-navbar-item>
+		<tv-navbar-item @on:action="setGenre('romance')"> Romance </tv-navbar-item>
+	</tv-navbar>
 	<tv-container></tv-container>
 </template>
---
+++ packages/web/src/components/navbar/TvNavbarItem.vue
@@ -0,0 +1,18 @@
+<script lang="ts" setup>
+import { TvButton } from '@components/button'
+
+const emits = defineEmits<{
+  (e: 'on:action', ...args): void
+}>()
+</script>
+<template>
+	<li class="navbar-item">
+		<tv-button @onPressed="emits('on:action')" class="navbar-item__button" variant="plain" in-dark-mode is-full-width>
+			<slot />
+		</tv-button>
+	</li>
+</template>
+
+<style lang="scss" scoped>
+@use '@/scss/6-components/navbar-item' as *;
+</style>
