f9c60f5eed584fbe5a352c4571570091e4d27c01
Oliver Arthur <OliverArthur@users.noreply.github.com>
2023-07-11 00:33:37 +0200

    defined the main layout

--- packages/web/src/components/layouts/MainLayout.vue
+++ packages/web/src/components/layouts/MainLayout.vue
@@ -16,36 +16,41 @@ const navbarOpen = ref(true)
 const toggleNavbar = () => {
 	navbarOpen.value = !navbarOpen.value
 }
-
 </script>
 <template>
-	<tv-header>
-		<template #burger-button>
-			<tv-button @on-pressed="toggleNavbar " variant="icon" icon-name="menu" />
-		</template>
-		<template #logo> SeriesTube </template>
-		<template #content>
-			<tv-autocomplete
-				:items="[
-					'banana',
-					'orange',
-					'pineapple',
-					'grape',
-					'strawberry',
-					'watermelon',
-					'melon',
-					'papaya'
-				]"
-			/>
-		</template>
-	</tv-header>
-	<tv-navbar :is-open="navbarOpen">
-		<tv-navbar-item @on:action="setGenre('action')"> Action </tv-navbar-item>
-		<tv-navbar-item @on:action="setGenre('adventure')"> Adventure </tv-navbar-item>
-		<tv-navbar-item @on:action="setGenre('drama')"> Drama </tv-navbar-item>
-		<tv-navbar-item @on:action="setGenre('horror')"> Horror </tv-navbar-item>
-		<tv-navbar-item @on:action="setGenre('mystery')"> Mystery </tv-navbar-item>
-		<tv-navbar-item @on:action="setGenre('romance')"> Romance </tv-navbar-item>
-	</tv-navbar>
-	<tv-container></tv-container>
+	<tv-container class="main-page">
+		<tv-header class="main-page__header">
+			<template #burger-button>
+				<tv-button @on-pressed="toggleNavbar" variant="icon" icon-name="menu" />
+			</template>
+			<template #logo> SeriesTube </template>
+			<template #content>
+				<tv-autocomplete
+					:items="[
+						'banana',
+						'orange',
+						'pineapple',
+						'grape',
+						'strawberry',
+						'watermelon',
+						'melon',
+						'papaya'
+					]"
+				/>
+			</template>
+		</tv-header>
+		<tv-navbar class="main-page__nav" :is-open="navbarOpen">
+			<tv-navbar-item @on:action="setGenre('action')"> Action </tv-navbar-item>
+			<tv-navbar-item @on:action="setGenre('adventure')"> Adventure </tv-navbar-item>
+			<tv-navbar-item @on:action="setGenre('drama')"> Drama </tv-navbar-item>
+			<tv-navbar-item @on:action="setGenre('horror')"> Horror </tv-navbar-item>
+			<tv-navbar-item @on:action="setGenre('mystery')"> Mystery </tv-navbar-item>
+			<tv-navbar-item @on:action="setGenre('romance')"> Romance </tv-navbar-item>
+		</tv-navbar>
+		<section class="main-page__content"></section>
+	</tv-container>
 </template>
+
+<style scoped lang="scss">
+@use '@/scss/7-modules/main-page' as *;
+</style>
